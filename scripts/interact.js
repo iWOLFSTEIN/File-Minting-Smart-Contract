f7d41a94bae46dfe3c1a305e75977601108f77a6
iWOLFSTEIN <mtalha7274@gmail.com>
2022-06-04 04:57:24 -0700

    minted files count is added

--- scripts/interact.js
+++ scripts/interact.js
@@ -20,17 +20,21 @@ const fileMinterContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, s
 async function main() {
     
   // use your own unique hash code
-    const fileHashCode = "0x185f8db32271fe25f561a6fc938b2e264306ec304eda508007d1764826381961";
+    const fileHashCode = "0x185f8db32271fe25f561a6fc938b2e264306ac304eda508007d1764826381963";
     console.log('file hash is');
     console.log(fileHashCode);
 
     await fileMinterContract.mintFile(signer.address, fileHashCode);
 
     const arrayOfHashes = await fileMinterContract.myMintFiles(signer.address);
+    var total = 0;
     for(var i = 0; i< arrayOfHashes.length; i++){
       console.log('file hash after minting is');
     console.log(arrayOfHashes[i]);
+    total++;
   }
+  console.log('total minted files are: '+ total.toString());
+
 }
 
 main()
